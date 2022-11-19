7c9d642f0dfa158164e391710983e8e13b8a4275
fusunnn <i.audouard22@ejm.org>
2022-11-19 18:31:37 +0100

    added minimal error handling for prisma

--- src/server/trpc/router/test.ts
+++ src/server/trpc/router/test.ts
@@ -5,11 +5,15 @@ import { prisma } from "../../db/client";
 export const testRouter = router({
   // Create publicProcedure at path 'hello'
   get: publicProcedure.query(async () => {
-    const tests = await prisma.test.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    const tests = await prisma.test
+      .findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+      .catch((err) => {
+        throw err;
+      });
     return {
       tests: tests,
     };
