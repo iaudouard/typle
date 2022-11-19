b8d00ff48159ff4d77accd38a7b812652e73b328
fusunnn <i.audouard22@ejm.org>
2022-11-19 17:55:38 +0100

    added cron job to update test

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+import path from "path";
+import { promises as fs } from "fs";
+import { createTest } from "../../server/test-utils/createTest";
+
+export default async function cron(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      const { authorization } = req.headers;
+
+      if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+        //Find the absolute path of the json directory
+        const jsonDirectory = path.join(process.cwd(), "json");
+        //Read the json data file data.json
+        const fileContents = await fs.readFile(
+          jsonDirectory + "/words.json",
+          "utf8"
+        );
+
+        const words: string[] = JSON.parse(fileContents).commonWords;
+
+        const newTest = createTest(words);
+
+        const pushTest = await prisma.test.create({
+          data: {
+            test: newTest,
+          },
+        });
+        res.status(200).json({ success: true });
+      } else {
+        res.status(401).json({ success: false });
+      }
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
--- src/server/trpc/router/test.ts
+++ src/server/trpc/router/test.ts
@@ -1,9 +1,6 @@
 import { router, publicProcedure } from "../trpc";
-import { z } from "zod";
+// import { z } from "zod";
 import { prisma } from "../../db/client";
-import path from "path";
-import { promises as fs } from "fs";
-import { createTest } from "../../test-utils/createTest";
 
 export const testRouter = router({
   // Create publicProcedure at path 'hello'
@@ -16,32 +13,5 @@ export const testRouter = router({
     return {
       tests: tests,
     };
-  }),
-  create: publicProcedure.query(async () => {
-    //Find the absolute path of the json directory
-    const jsonDirectory = path.join(process.cwd(), "json");
-    //Read the json data file data.json
-    const fileContents = await fs.readFile(
-      jsonDirectory + "/words.json",
-      "utf8"
-    );
-
-    const words: string[] = JSON.parse(fileContents).commonWords;
-    // const newTest = createTest()
-
-    const newTest = createTest(words);
-    // console.log(newTest);
-
-    const pushTest = await prisma.test.create({
-      data: {
-        test: newTest,
-      },
-    });
-
-    return {
-      words: fileContents,
-    };
-
-    // const addTest = await prisma.test.
   }),
 });
