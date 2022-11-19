8f5231d353ea3c94f1b842b9fa590ede4fb09410
fusunnn <i.audouard22@ejm.org>
2022-11-19 17:36:43 +0100

    added text rendering logic

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -27,8 +27,15 @@ export const Test = (props: Props) => {
         }`}
       >
         <h3 className="user-none cursor-default text-2xl font-semibold">
-          {props.testString?.split("").map((char, i) => {
-            return <Char userInput={userInput} char={char} index={i} key={i} />;
+          {props.testString?.split(" ").map((testWord, i) => {
+            return (
+              <Word
+                userInput={userInput}
+                testWord={testWord}
+                index={i}
+                key={i}
+              />
+            );
           })}
         </h3>
       </div>
@@ -48,40 +55,49 @@ export const Test = (props: Props) => {
   );
 };
 
-// type WordProps = {
-//   userInput: string;
-//   word: string;
-//   index: number;
-// };
+type WordProps = {
+  userInput: string;
+  testWord: string;
+  index: number;
+};
 
-// const Word = (props: WordProps) => {
-//   return (
-//     <span>
-//       {props.word.split("").map((char, j) => {
-//         return (
-//           <Char userInput={props.userInput.split(" ")} char={char} index={j} key={j} />
-//         );
-//       })}{" "}
-//     </span>
-//   );
-// };
+const Word = (props: WordProps) => {
+  const userInputWordList = props.userInput.split(" ");
+  return (
+    <span>
+      {props.testWord.split("").map((char, j) => {
+        return (
+          <Char
+            userInput={userInputWordList[props.index]}
+            char={char}
+            index={j}
+            key={j}
+          />
+        );
+      })}{" "}
+    </span>
+  );
+};
 
 type CharProps = {
-  userInput: string;
+  userInput: string | undefined;
   char: string;
   index: number;
 };
 
 const Char = (props: CharProps) => {
-  const charColor = props.userInput.charAt(props.index)
-    ? props.userInput.charAt(props.index) === props.char
-      ? "white"
-      : "blue-500"
-    : "gray-500";
+  const charClass = props.userInput
+    ? props.userInput.charAt(props.index)
+      ? props.userInput.charAt(props.index) === props.char
+        ? "char-correct"
+        : "char-incorrect"
+      : "char-inactive"
+    : "char-inactive";
+
   return (
     <span
       key={props.index}
-      className={`transition-color duration-200 text-${charColor}`}
+      className={`transition-color duration-200 ${charClass}`}
     >
       {props.char}
     </span>
