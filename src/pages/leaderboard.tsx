bbf0b42a6803763b966b45818974c9eca0e165b9
fusunnn <i.audouard22@ejm.org>
2022-12-14 05:43:33 +0100

    styled leaderboard a bit

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
 import Head from "next/head";
 import React, { useContext } from "react";
 import { Loading } from "../components/Loading";
+import { transition, variants } from "../constants/animation-values";
 import { TestContext } from "../context/TestContext";
 import { trpc } from "../lib/trpc";
 
@@ -22,15 +24,30 @@ const Leaderboard = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="page-container items-center justify-center">
-        <ul>
-          {leaderboard.data.leaderboard.map((testResult, i) => {
-            return (
-              <li key={i} className="text-2xl font-semibold text-white">
-                {`${i + 1}. ${testResult.wpm}`}
-              </li>
-            );
-          })}
-        </ul>
+        {/* leaderboard list section */}
+        <motion.section
+          variants={variants}
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          transition={transition}
+          className="w-1/3"
+        >
+          <ul>
+            {leaderboard.data.leaderboard.map((testResult, i) => {
+              return (
+                <li
+                  key={i}
+                  className="my-4 flex items-center justify-between rounded-md bg-gray-400 py-2 px-4 text-2xl font-semibold text-white"
+                >
+                  <p>{`${i + 1}. ${testResult.wpm}  `}</p>
+
+                  <span className="italic">{`(${testResult.username})`}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </motion.section>
       </main>
     </>
   );
