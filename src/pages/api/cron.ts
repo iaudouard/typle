b279f714697c6ed95696742553f46ee8a83631c9
Ivan Audouard <ivanaudouard@Ivans-MacBook-Pro.local>
2023-02-22 23:07:03 +0900

    removed username local storage stuff

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,41 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../server/db/client";
-import path from "path";
 import { promises as fs } from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import { prisma } from "../../server/db/client";
 import { createTest } from "../../server/test-utils/create-test";
 
 export default async function cron(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      const { authorization } = req.headers;
-
-      if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
-        //Find the absolute path of the json directory
-        const jsonDirectory = path.join(process.cwd(), "json");
-        //Read the json data file data.json
-        const fileContents = await fs.readFile(
-          jsonDirectory + "/words.json",
-          "utf8"
-        );
-
-        const words: string[] = JSON.parse(fileContents).commonWords;
-
-        const newTest = createTest(words);
-
-        await prisma.test.create({
-          data: {
-            test: newTest,
-          },
-        });
-        res.status(200).json({ success: true, test: newTest });
-      } else {
-        res.status(401).json({ success: false });
-      }
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err });
-    }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+  if (req.query.key !== process.env.CRON_KEY) {
+    res.status(404).end();
+    return;
   }
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + "/words.json", "utf8");
+
+  const words: string[] = JSON.parse(fileContents).commonWords;
+
+  const newTest = createTest(words);
+
+  await prisma.test.create({
+    data: {
+      test: newTest,
+    },
+  });
+  res.status(200).json({ success: true, test: newTest });
 }
